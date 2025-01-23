2dc34596db33f3dc0838d97d07318c67f4522fbf
Colin Shea <colin.shea@myfwc.com>
2025-01-23 17:34:24 -0500

    deleted a redundant function

--- R/filterOnlyDataAllowed.R
+++
@@ -1,6 +0,0 @@
-#' Filter valid base pair combinations
-#' @description This function is used to select only only columns that contain allowable characters (those listed in IUPAC)
-#' by the convertBasePairstoAlleles function. DOESNT THIS JUST RETURN THE ALLELEPAIRS?
-#' @param x the dataset to use
-#' @export
-filterOnlyDataAllowed <- function(x) {x %in% IUPAC$Allelepairs}
