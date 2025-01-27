4c871f5c4f9fc59910757b907a7ea3f884b5e4de
Colin Shea <colin.shea@myfwc.com>
2025-01-26 20:48:13 -0500

    More

--- R/runKinship.R
+++ R/runKinship.R
@@ -32,7 +32,7 @@ runKinship <- function(subset = FALSE, targetN = NULL){
     d <- kinshipCalcsNoInvar(dataset = c, targetN = targetN, subset = subset)
     if (subset==FALSE){
     write.csv(d$PopAvgMKGD, paste0(resultsLocation,"/","popAvgMKGD_", paste0(fileList[[i]])), row.names = F)
-    write.csv(d$MK_init, paste0(resultsLocation,"/","kinship_Init_", nrow(b), "_", paste0(fileList[[i]])), row.names = F)
+    write.csv(d$MK_init, paste0(resultsLocation,"/","kinship_Init_", paste0(fileList[[i]])), row.names = F)
     }
     if (subset==TRUE){
     write.csv(d$PopAvgMKGD, paste0(resultsLocation,"/","popAvgMKGD_", paste0(fileList[[i]])), row.names = F)
