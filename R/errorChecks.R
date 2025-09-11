f414ffafe3001309457d1408a727d8b278e8f7df
Shea <colin.shea@myfwc.com>
2025-09-11 10:30:16 -0400

    Added to error message

--- R/errorChecks.R
+++ R/errorChecks.R
@@ -47,5 +47,5 @@ handleError_ProhibitedData <- function(dataset, acceptableData) {
 #' @export
 find_dups <- function(df){
   IDs <- df %>% group_by(Coral_ID) %>% summarise(N = n()) %>% filter(N>1) %>% pull(Coral_ID)
-  if(length(IDs)>0) {stop(cat("At least one colony has multiple versions of SNP data which is not permitted. The offending colonies are:", IDs, sep = "\n"))}
+  if(length(IDs)>0) {stop(cat("At least one colony has multiple versions of SNP data which is not permitted. Contact the coral section to see if these are ramets or re-samples that can be re-named. The offending colonies are:", IDs, sep = "\n"))}
 }
